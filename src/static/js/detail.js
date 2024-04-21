223553a7cb1730b06568fd5714e4162d2dd4fba8
Le0-dot <lkolyadich@gmail.com>
2024-04-21 18:09:45 +0200

    Add ability to exclude whole tags from selectors (only ids and classes before)

--- src/static/js/detail.js
+++ src/static/js/detail.js
@@ -3,14 +3,17 @@ function selectorToStr() {
     const selector = [];
     for(var i = 0; i < rows.length; ++i) {
         const [tag, id, classes] = rows[i].children;
-        var tag_info = [tag.textContent];
+        if(!tag.children[0].checked) {
+            continue;
+        }
+        var tag_info = [tag.children[1].textContent.trim()];
         if (id.children.length != 0 && id.children[0].checked) {
-            tag_info.push("#" + id.children[1].textContent);
+            tag_info.push("#" + id.children[1].textContent.trim());
         }
         for (var j = 0; j < classes.children.length; ++j) {
             class_ = classes.children[j];
             if(class_.children[0].checked) {
-                tag_info.push("." + class_.children[1].textContent);
+                tag_info.push("." + class_.children[1].textContent.trim());
             }
         }
         selector.push(tag_info.join(""));
