52e1a00bb3cc0593567fa54520c39037115a1a15
Le0-dot <lkolyadich@gmail.com>
2024-04-25 19:29:02 +0200

    Add use of url_for in details template

--- src/static/js/selector_to_str.js
+++ src/static/js/selector_to_str.js
@@ -1,12 +1,12 @@
 function selectorToStr() {
-    var rows = document.getElementById("selector-table-body").children;
+    const rows = document.getElementById("selector-table-body").children;
     const selector = [];
     for(var i = 0; i < rows.length; ++i) {
         const [tag, id, classes] = rows[i].children;
         if(!tag.children[0].checked) {
             continue;
         }
-        var tag_info = [tag.children[1].textContent.trim()];
+        const tag_info = [tag.children[1].textContent.trim()];
         if (id.children.length != 0 && id.children[0].checked) {
             tag_info.push("#" + id.children[1].textContent.trim());
         }
@@ -19,10 +19,4 @@ function selectorToStr() {
         selector.push(tag_info.join(""));
     }
     return selector.join(" ");
-}
-
-function goToNext() {
-    var selector = selectorToStr();
-    selector = encodeURIComponent(selector);
-    window.location.href = '/select/next?selector=' + selector;
 }
