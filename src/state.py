c4c1ee15ea3222724cde453f0a94c8f387b8651d
Le0-dot <lkolyadich@gmail.com>
2024-04-26 20:26:31 +0200

    Create State class that is a wrapper around session dict object

--- src/state.py
+++ src/state.py
@@ -1,24 +1,90 @@
 from typing import Any
 
-
-def init(session: dict[str, Any]) -> None:
-    session["step"] = 0
-    session["selectors"] = []
+from fastapi import Request
 
 
-def select(session: dict[str, Any], url: str) -> None:
-    session["url"] = url
+class DictObject:
+    def __init__(self, data: dict[Any, Any]) -> None:
+        super().__setattr__("data", data)
+
+    def _allowed_attr(self, name: str) -> bool:
+        return True
+
+    def __getattr__(self, name: str) -> str:
+        if self._allowed_attr(name):
+            raise AttributeError(f'No attribute {name} found in class {self.__class__}')
+        try:
+            return super().__getattribute__("data")[name]
+        except KeyError:
+            raise AttributeError(f'No attribute {name} found in class {self.__class__}')
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        if self._allowed_attr(name):
+            raise AttributeError(f'No attribute {name} found in class {self.__class__}')
+        super().__getattribute__("data").__setitem__(name, value)
 
 
-def push(session: dict[str, Any], selector: str) -> bool:
-    session["step"] += 1
-    session["selectors"].append(selector)
-    return session["step"] < 3
+class Extractor(DictObject):
+    def _allowed_attr(self, name: str) -> bool:
+        return name in ['name', 'selector']
 
 
-def get_url(session: dict[str, Any]) -> str:
-    return session["url"]
+class State:
+    def __init__(self, request: Request) -> None:
+        self.__session = request.session
 
+    def init(self, url: str, delay: int) -> None:
+        self.url = url
+        self.delay = delay
+        self.extractors = []
+        self.current_extractor_idx = None
 
-def get_selectors(session: dict[str, Any]) -> list[str]:
-    return session["selectors"]
+    @property
+    def url(self) -> str:
+        return self.__session["url"]
+
+    @url.setter
+    def url(self, value: str) -> None:
+        self.__session["url"] = value
+
+    @property
+    def delay(self) -> int:
+        return self.__session["delay"]
+
+    @delay.setter
+    def delay(self, value: int) -> None:
+        self.__session["delay"] = value
+
+    @property
+    def extractors(self) -> list[Extractor]:
+        return list(map(Extractor, self.__session["extractors"]))
+
+    @extractors.setter
+    def extractors(self, values: list[Extractor]) -> None:
+        self.__session["extractors"] = [do.data for do in values]
+
+    @property
+    def current_extractor_idx(self) -> int | None:
+        return self.__session["current_extractor_idx"]
+
+    @current_extractor_idx.setter
+    def current_extractor_idx(self, value: int | None) -> None:
+        self.__session["current_extractor_idx"] = value
+
+    @current_extractor_idx.deleter
+    def current_extractor_idx(self) -> None:
+        self.__session["current_extractor_idx"] = None
+
+    @property
+    def current_extractor(self) -> Extractor | None:
+        idx = self.current_extractor_idx
+        return Extractor(self.__session["extractors"][idx])
+
+    @current_extractor.setter
+    def current_extractor(self, value: Extractor) -> None:
+        idx = self.current_extractor_idx
+        self.__session["extractors"][idx] = value.data
+
+    @current_extractor.deleter
+    def current_extractor(self) -> None:
+        del self.current_extractor_idx
