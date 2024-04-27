0cc3e0e8cebe18ac80560616844af236192785d9
Le0-dot <lkolyadich@gmail.com>
2024-04-27 19:57:12 +0200

    Show all created extractors on recipe page. Fix redirect url. Add endpoints for changing name and deletion of extractors. Add id query parameter to select.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
+from typing import Any
 from random import randint
 from urllib.parse import unquote
 
-from fastapi import Depends, FastAPI, Request, status
+from fastapi import Depends, FastAPI, Request, Response, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 from selenium.webdriver.ie.webdriver import WebDriver
 from starlette.middleware.sessions import SessionMiddleware
 
 import parser
-from state import State
+from state import State, Extractor
 from selector import parse_selector
 from cleaner import get_clean, get_context
 from resources import Templates, Driver
@@ -38,33 +40,66 @@ def init(request: Request, url: str, delay: int):
 
 @app.get("/recipe", response_class=HTMLResponse)
 def recipe(request: Request, templates: Jinja2Templates = Depends(Templates.get)):
+    extractors = map(Extractor, State(request).extractors)
+    contexts = (e.to_dict(request=request) for e in extractors)
+
+    extractor_tempalte = templates.get_template("extractor.html")
+    rendered = map(extractor_tempalte.render, contexts)
+
     return templates.TemplateResponse(
         name="recipe.html",
+        context={"extractors": rendered},
         request=request,
     )
 
 
 @app.get("/extractor", response_class=HTMLResponse)
 def extractor(request: Request, templates: Jinja2Templates = Depends(Templates.get)):
+    extractors = State(request).extractors
+    extractor = Extractor()
+    extractors.append(extractor.data)
     return templates.TemplateResponse(
         name="extractor.html",
-        context={"idx": 0},
+        context={"id": extractor.id},
         request=request,
     )
 
 
-@app.put("/recipe/current", status_code=status.HTTP_201_CREATED)
+@app.put("/recipe/current", status_code=status.HTTP_204_NO_CONTENT)
 def current(request: Request, idx: int):
-    State(request).current_extractor_idx = idx
+    State(request).current_extractor_id = idx
+
+
+class PutNamePayload(BaseModel):
+    name: str
+
+@app.put("/recipe/extractor/{id}/name", status_code=status.HTTP_204_NO_CONTENT)
+def put_name(request: Request, id: int, data: PutNamePayload):
+    extractor = State(request).extractor_by_id(id)
+    if extractor is None:
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
+    extractor.name = data.name
+
+
+@app.delete("/recipe/extractor/{id}", status_code=status.HTTP_200_OK)
+def remove(request: Request, id: int):
+    state = State(request)
+    idx = state.extractor_idx_by_id(id)
+    if idx is None:
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
+    del state.extractors[idx]
 
 
 @app.get("/select", response_class=HTMLResponse)
 def select(
     request: Request,
+    id: int,
     templates: Jinja2Templates = Depends(Templates.get),
     driver: WebDriver = Depends(Driver.get),
 ):
-    url = State(request).url
+    state = State(request)
+    state.current_extractor_id = id
+    url = state.url
     return templates.TemplateResponse(
         name="selector.html",
         context=get_context(url, driver),
@@ -92,9 +127,9 @@ def next(request: Request, selector: str):
     state.current_extractor.selector = selector
     del state.current_extractor
 
-    return request.url_for("recipe")
+    return str(request.url_for("recipe"))
 
-#
+
 # @app.get("/select/confirm", response_class=HTMLResponse)
 # def confirm(
 #     request: Request,
